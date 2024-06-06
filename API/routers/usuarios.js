b77aa9bfaa1f29183133d3fe7d84bddd68ebb44c
Carlos Eduardo <132010811+kadujms@users.noreply.github.com>
2024-06-06 14:16:41 -0300

    Delete API directory

--- API/routers/usuarios.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/usuarios');
-
-router.get('/usuarios', userController.getAllUsers);
-router.delete('/usuarios/:email', userController.deleteUser)
-module.exports = router;
